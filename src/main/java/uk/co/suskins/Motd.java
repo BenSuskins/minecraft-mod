2a5426947e970e55627f1032e735c6238156a2e1
Ben Suskins <suskinsdevelopment@gmail.com>
2024-09-10 21:59:37 +0100

    Initial commit

--- src/main/java/uk/co/suskins/Motd.java
+++ src/main/java/uk/co/suskins/Motd.java
@@ -1,52 +1,50 @@
 package uk.co.suskins;
 
-import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
-import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 
 public class Motd implements ModInitializer {
 
-	private String motd = "Welcome to the server!"; // Default MOTD
-	private MinecraftServer server;
+    private String motd = "Welcome to the server!"; // Default MOTD
+    private MinecraftServer server;
 
-	@Override
-	public void onInitialize() {
-		// Register commands to set the MOTD
-		CommandRegistrationCallback.EVENT.register(this::registerCommands);
+    @Override
+    public void onInitialize() {
+        // Register commands to set the MOTD
+        CommandRegistrationCallback.EVENT.register(this::registerCommands);
 
-		// Listen for when a player joins
-		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-			ServerPlayerEntity player = handler.getPlayer();
-			player.sendMessage(Text.literal(motd), false);
-		});
+        // Listen for when a player joins
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            ServerPlayerEntity player = handler.getPlayer();
+            player.sendMessage(Text.literal(motd), false);
+        });
 
-		// Listen for server start to set the server object
-		ServerLifecycleEvents.SERVER_STARTED.register(server -> this.server = server);
-	}
+        // Listen for server start to set the server object
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> this.server = server);
+    }
 
-	// Register a command to change the MOTD
-	private void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher, CommandManager.RegistrationEnvironment environment) {
-		dispatcher.register(CommandManager.literal("setmotd")
-				.requires(source -> source.hasPermissionLevel(2)) // Requires OP level 2 (Server Operators)
-				.then(CommandManager.argument("motd", StringArgumentType.greedyString())
-						.executes(context -> {
-							motd = StringArgumentType.getString(context, "motd");
-							context.getSource().sendFeedback(Text.literal("MOTD set to: " + motd), true);
-							return 1;
-						})
-				)
-		);
-	}
+    private void registerCommands(CommandDispatcher<ServerCommandSource> serverCommandSourceCommandDispatcher,
+                                  CommandRegistryAccess commandRegistryAccess,
+                                  CommandManager.RegistrationEnvironment registrationEnvironment) {
+        serverCommandSourceCommandDispatcher.register(CommandManager.literal("setmotd")
+                .requires(source -> source.hasPermissionLevel(2)) // Requires OP level 2 (Server Operators)
+                .then(CommandManager.argument("motd", StringArgumentType.greedyString())
+                        .executes(context -> {
+                            motd = StringArgumentType.getString(context, "motd");
+                            context.getSource().sendFeedback(() -> Text.literal("MOTD set to: " + motd), true);
+                            return 1;
+                        })
+                )
+        );
+    }
 }
